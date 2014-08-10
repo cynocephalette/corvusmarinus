e40cef841fc9d470a3c0f9b0ba603d5a18438138
cynocephalette <corvusmarinusresident@gmail.com>
2014-08-09 18:13:20 -0700

    Static site

--- corvus.py
+++ corvus.py
@@ -1,11 +1,15 @@
 import os
-from flask import Flask
+from flask import Flask, request, make_response, g
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
-db = SQLAlchemy(app)
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
+#db = SQLAlchemy(app)
 
 @app.route('/')
 def corvus():
-    return 'corvus marinus'
+    return make_response(open('index.html').read())
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run(port=5000)
---
+++ static/scripts/display.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    var headers = $('header, .subheader');
+    console.log(headers);
+    headers.each(function (index) {
+        var header = $(headers[index]);
+        var next = headers[index + 1];
+        header.scrollToFixed({
+            limit: function () {
+                var limit = 0;
+                if (next) {
+                    limit = $(next).offset().top - $(this).outerHeight(true);
+                } else {
+                    limit = 0 - $(this).outerHeight(true);
+                }
+                return limit;
+            },
+            zIndex: 999
+        });
+    });
+});
