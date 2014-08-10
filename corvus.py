25276fb521e8ab176826dc21df661d86420f1f3e
cynocephalette <corvusmarinusresident@gmail.com>
2014-08-10 15:36:22 -0700

    Adds library stub, tidies up css

--- corvus.py
+++ corvus.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, request, make_response, g
+from flask import Flask, request, make_response, render_template, g
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,6 +9,11 @@ app = Flask(__name__)
 @app.route('/')
 def corvus():
     return make_response(open('index.html').read())
+
+@app.route('/library')
+def library():
+    books = [{'name': 'The Lesser Key of Solomon'}, {'name': 'Bluh'}]
+    return render_template('library.html', books=books)
 
 if __name__ == '__main__':
     app.debug = True
