d01bef3a4cf219faa9b1e833725fbffa258439cb
cynocephalette <corvusmarinusresident@gmail.com>
2014-08-10 16:51:53 -0700

    Adds library carousel

--- corvus.py
+++ corvus.py
@@ -2,6 +2,8 @@ import os
 from flask import Flask, request, make_response, render_template, g
 from flask.ext.sqlalchemy import SQLAlchemy
 
+from books import books
+
 app = Flask(__name__)
 #app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['DATABASE_URL']
 #db = SQLAlchemy(app)
@@ -12,7 +14,6 @@ def corvus():
 
 @app.route('/library')
 def library():
-    books = [{'name': 'The Lesser Key of Solomon'}, {'name': 'Bluh'}]
     return render_template('library.html', books=books)
 
 if __name__ == '__main__':
